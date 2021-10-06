3cff3b59d9ba152b7757e022aa227df5f4f9022f
josh.94 <jeshua.cabanillas@gmail.com>
2021-10-06 20:12:38 +0000

    First try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: An integer
+ * @b: An integer
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int (suma);
+	suma = a + b;
+	return (suma);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char c);
+int add(int, int);
