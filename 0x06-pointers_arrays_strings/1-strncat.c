2d1812618d62f5c7f5c468e4dc95e155399c7722
josh.94 <jeshua.cabanillas@gmail.com>
2021-10-15 16:51:33 +0000

    Correcting code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,14 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int c = 0;
 	int count = 0;
-	int j = 0;
 
 	while (dest[c] != '\0')
 	{
 		c++;
 	}
-	while (src[count] != '\0')
+	while (count < n && src[count] != '\0')
 	{
 		dest[c] = src[count];
-		j++;
-
-		if (j >= n)
-			break;
-
 		count++;
 		c++;
 	}
