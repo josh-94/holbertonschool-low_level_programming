fcdca3d7cd81125ef2219cbd49d6a55d99bb8256
josh.94 <jeshua.cabanillas@gmail.com>
2021-10-15 16:43:33 +0000

    First commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strcat - function that concatenates two strings
+ *@dest: input string
+ *@src: input string
+ *@n: limits numbers characters to add
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c = 0;
+	int count = 0;
+	int j = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	while (src[count] != '\0')
+	{
+		dest[c] = src[count];
+		j++;
+
+		if (j >= n)
+			break;
+
+		count++;
+		c++;
+	}
+
+	return (dest);
+
+}
