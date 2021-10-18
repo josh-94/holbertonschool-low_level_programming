f975a2b87acb15ec37a914a7270a5ee5b0ff88cc
josh.94 <jeshua.cabanillas@gmail.com>
2021-10-18 03:44:20 +0000

    Second try

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar (i);
-		putchar (',');
-		putchar (' ');
+		if (i < 57)
+		{
+			putchar (',');
+			putchar (' ');
+		}
 	}
-	putchar (i);
 	putchar('\n');
 	return (0);
 }
