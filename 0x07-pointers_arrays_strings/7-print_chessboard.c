7688cf6ba40b99149c9e2616ffcd84ad37bc2308
josh.94 <jeshua.cabanillas@gmail.com>
2021-10-19 16:58:31 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print given chessboard
+ *
+ * @a: two dimensional array - 8x8
+ *
+ * Return: none - void function
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; i < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
