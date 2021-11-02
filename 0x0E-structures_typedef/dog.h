6a7eae72b8a3975bfa60b6c0742e5ed5d506da7e
josh-94 <jeshua.cabanillas@gmail.com>
2021-11-02 11:24:45 -0500

    First commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - it's an structure
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
