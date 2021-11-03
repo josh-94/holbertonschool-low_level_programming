8fa63da7ea4c282fb4c99285b28493a974068d19
josh-94 <jeshua.cabanillas@gmail.com>
2021-11-02 22:52:56 -0500

    Correcting betty style

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,10 +3,10 @@
 
 /*
  * File: 3-function_like_macro.h
- * Desc: 
+ * Desc: function-like macro ABS(x) that computes
+ *the absolute value of a number x.
  *
  */
-
-#define ABS(x) ((x)<0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
