f8145858a8013f3560ec802f7d72e03ef74f7d38
josh-94 <jeshua.cabanillas@gmail.com>
2021-11-04 19:39:55 -0500

    First commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+
+	_putchar(i);
+	_putchar(10); /*_putchar('\n');*/
+
+
+}
