3443a9418cd357c269fbe1995d651c7b073544a0
josh-94 <jeshua.cabanillas@gmail.com>
2021-11-11 17:49:02 -0500

    Correcting code & betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,29 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stddef.h>
+#include "lists.h"
 /**
+ * print_list - A function that prints all the elements
+ * of a list_t list.
+ * @h: A constant variable of type list_t.
  *
+ * Return: The number of nodes (i).
  *
  */
 size_t print_list(const list_t *h)
 {
 	const list_t *node = h;
-	int count = 0;
-	while(node->next != NULL)
+	unsigned int count = 0;
+
+	while (node != NULL)
 	{
+	if (node->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%d] %s\n", node->len, node->str);
+	}
 		count++;
 		node = node->next;
 	}
