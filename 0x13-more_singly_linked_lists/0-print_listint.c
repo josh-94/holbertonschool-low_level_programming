e439841f7a3a01b95ca12625695323f3e46836d1
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-16 09:30:06 -0500

    First commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef MORE_LINKED_LIST_H
+#define MORE_LINKED_LIST_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+#endif
