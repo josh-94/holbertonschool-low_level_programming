6ad1a369918556292a29e5667a94a47d20a33e8b
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-25 15:34:08 -0500

    First try

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
+#ifndef MAIN_FILE_FUNCTIONS_NESTED
+#define MAIN_FILE_FUNCTIONS_NESTED
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int add(int, int);
 int _islower(int c);
+int _isalpha(int c);
+#endif
