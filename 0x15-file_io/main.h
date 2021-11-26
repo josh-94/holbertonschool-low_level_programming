96449e17fd3613e2260ad32918204336880e8784
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-26 15:42:03 -0500

    Updating main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_FILE_I_O
 #define MAIN_FILE_I_O
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdlib.hi>
+#include <sys/types.h>
+#include <sys/stat.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
