32cd34919d549ef45a7c7d2566c23a0c414dc4eb
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-26 15:19:49 -0500

    First commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_FILE_I_O
+#define MAIN_FILE_I_O
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
