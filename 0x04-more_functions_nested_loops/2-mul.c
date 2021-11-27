6e7782232a5ee55986a6a83201d55d59a85b8b78
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-26 22:23:22 -0500

    Correcting betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _mul - function that multiplies two integers.
+ * mul - function that multiplies two integers.
  * @a: integer to be multiplied.
  * @b: integer to be multiplied.
  * Return: Numbers multiplied.
