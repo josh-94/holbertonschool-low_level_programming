a2fe632a3f80a9d08e6de8849682c058cd984897
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-29 12:34:17 -0500

    Correcting code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - prints the numbers 1 - 100.
  * Fizz for multiple of 3, Buzz for multiples of 5,
@@ -14,7 +13,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
