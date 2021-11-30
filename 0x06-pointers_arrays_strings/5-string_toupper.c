56edf0ddf3e6328631e1459c55b15f781240ad84
josh.94 <jeshua.cabanillas@gmail.com>
2021-11-30 09:51:45 -0500

    Test 1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes lowercase to uppercase.
+ * @str: the string to change.
+ * Return: the string changed to uppercase.
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+		return (str);
+}
