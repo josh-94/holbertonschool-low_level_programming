15fa2b441ca9a0c0aaaf5e8023bde54d9d209ca5
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-01 11:09:14 -0500

    First commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @s: string to calculate substring length from.
+ * @accept: string containing bytes to match.
+ * Return: a pointer to the byte in s that matches one of the byte
+ * in accept, or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
-
+#include <stddef.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void print_chessboard(char (*a)[8]);
@@ -8,4 +8,6 @@ int _putchar(char c);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
 #endif
