478af3432d19b2290e8460b11ebc25019b0e0870
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-10 16:49:07 -0500

    First try

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>
 /**
  * struct dog - it's an structure
  * @name: name's dog
@@ -13,4 +14,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
