6ba44a427bd2d09bf222b5d5025d90b7f674ea41
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-17 11:41:54 -0500

    Working on NULL case

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int dec;
 	unsigned int a;
 
-	if (b == NULL)
-		return (0);
 	for (dec = 0, a = 0; b[a] != '\0'; a++)
 	{
+		if (b == NULL)
+			return (0);
 		if (b[a] == '1')
 			dec = (dec << 1) | 1;
 		else if (b[a] == '0')
