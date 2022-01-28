6f6e9defe3db30c36c6fcc763f1d3e4c3f710a73
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-28 11:13:46 -0500

    chaging folders

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: size of the array.
+ * Return: a pointer to the newly created hash table.
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hashtable;
+
+	new_hashtable = malloc(sizeof(hash_table_t));
+	if (new_hashtable == NULL)
+		return (NULL);
+	new_hashtable->array = malloc(sizeof(hash_node_t*) * size);
+	if (new_hashtable->array == NULL)
+		return(NULL);
+	return (new_hashtable);
+
+
+}
+
