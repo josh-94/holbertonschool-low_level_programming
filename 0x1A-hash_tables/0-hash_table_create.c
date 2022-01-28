1eaf74821f56c3411ddd7f4f0d62b4f2d0758626
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-28 11:59:21 -0500

    leak memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hashtable;
+	unsigned long int i = 0;
 
 	new_hashtable = malloc(sizeof(hash_table_t));
 	if (new_hashtable == NULL)
 		return (NULL);
+	new_hashtable->size = size;
 	new_hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hashtable->array == NULL)
 	{
 		free(new_hashtable);
 		return (NULL);
 	}
-	free(new_hashtable);
+	for (i = 0; i < size; i++)
+		new_hashtable->array[i] = NULL;
 	return (new_hashtable);
 
 
