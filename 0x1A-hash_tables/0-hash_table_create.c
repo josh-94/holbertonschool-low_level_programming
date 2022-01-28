5288142c276a0d7eb838be58191cf84512e373c2
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-28 12:26:24 -0500

    fix: memory leak

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,26 +3,22 @@
  * hash_table_create - function that creates a hash table.
  * @size: size of the array.
  * Return: a pointer to the newly created hash table.
- **/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hashtable;
-	unsigned long int i = 0;
+	hash_table_t *new_hashtable = NULL;
 
-	new_hashtable = malloc(sizeof(hash_table_t));
+	if (size <= 0)
+		return (NULL);
+	new_hashtable = malloc(sizeof(hash_table_t) * 1);
 	if (new_hashtable == NULL)
 		return (NULL);
 	new_hashtable->size = size;
-	new_hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	new_hashtable->array = calloc(size, sizeof(hash_node_t *));
 	if (new_hashtable->array == NULL)
 	{
 		free(new_hashtable);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		new_hashtable->array[i] = NULL;
 	return (new_hashtable);
-
-
 }
-
