babc0a888d1be1cc70aecac4837da04b203a0eb0
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-03 17:40:31 -0500

    Add: task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+ * int linear_search - function that searches for a value in an array of integers using the Linear search algorithm.
+ * @array:  is a pointer to the first element of the array to search in.
+ * @size: is the number of elements in array.
+ * @value: is the value to search for.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+}
