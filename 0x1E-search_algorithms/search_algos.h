11141612a386bd758b23ab7e07d3f99e4805fa36
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-04 18:54:01 -0500

    update: new functions header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,7 @@
 
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_arr(int *array, int left, int right);
+
 #endif
